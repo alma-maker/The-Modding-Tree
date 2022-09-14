3942d3cce591ed6be5e39fd80c61dd92ba21a9d5
alma-maker <sajt32.fanvok@gmail.com>
2022-09-14 18:44:38 +0200

    e
    a

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The apple Tree",
+	id: "42069hahaha",
+	author: "alma-maker",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
